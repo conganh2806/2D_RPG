a4207f85bc5d01c4f0869897923f0896f91d28f4
Hoang Nguyen C <gametamin.gd2@gmail.com>
2023-08-29 19:29:04 +0700

    doing wander script

--- Assets/Scripts/MonoBehaviours/Wander.cs
+++ Assets/Scripts/MonoBehaviours/Wander.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 
 
 [RequireComponent(typeof(CircleCollider2D))]
@@ -8,5 +10,79 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class Wander : MonoBehaviour
 {
-    
+    [SerializeField] private float pursuitSpeed;
+
+    [SerializeField] private float wanderSpeed;
+    [SerializeField] private float currentSpeed;
+
+    [SerializeField] private float directionChangeInterval;
+
+    public bool followPlayer;
+
+    Coroutine moveCoroutine;
+
+    [SerializeField] private Rigidbody2D rb2d;
+    [SerializeField] private Animator animator;
+
+    private Transform targetTransform = null;
+
+    private Vector3 endPosition;
+
+    private float currentAngle = 0;
+
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
+        currentSpeed = wanderSpeed;
+        StartCoroutine(WanderRoutine());
+
+    }
+
+    private IEnumerator WanderRoutine()
+    {
+        while(true)
+        {
+            ChooseNewEndPoint();
+
+            if(moveCoroutine != null)
+            {
+                StopCoroutine(moveCoroutine);
+            }
+
+            
+            //moveCoroutine = StartCoroutine(Move(rb2d, wanderSpeed));
+            
+
+
+            yield return new WaitForSeconds(directionChangeInterval);
+
+        }
+    }
+
+    private void ChooseNewEndPoint()
+    {
+        currentAngle += UnityEngine.Random.Range(0, 360);
+        currentAngle = Mathf.Repeat(currentAngle, 360);
+
+        endPosition += Vector3FromAngle(currentAngle);
+
+    }
+
+    Vector3 Vector3FromAngle(float inputAngleDegrees)
+    {
+        // 1
+        float inputAngleRadians = inputAngleDegrees * Mathf.Deg2Rad;
+        // 2
+        return new Vector3(Mathf.Cos(inputAngleRadians),
+       Mathf.Sin(inputAngleRadians), 0);
+    }
+
+
+    //private IEnumerator Move(Rigidbody2D rb2d, float wanderSpeed)
+    //{
+
+    //}
 }
